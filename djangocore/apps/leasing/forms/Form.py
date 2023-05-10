bb9fc1c0066cefe754f658398be309d6241aa622
Thiago Fernandes de Sousa <thiago.sousa@ifg.edu.br>
2023-05-10 16:46:45 -0300

    crud contrato de locação

--- djangocore/apps/leasing/forms/Form.py
+++ djangocore/apps/leasing/forms/Form.py
@@ -28,24 +28,6 @@ class ObjetoContratoForm(forms.ModelForm):
 
 
 
-'''
- numero = models.CharField(max_length=200)
-    tipo = models.ForeignKey(ObjetoContrato, related_name='tipo_contrato', on_delete=models.CASCADE )
-    objeto_contrato = models.ForeignKey(ObjetoContrato, related_name='objetocontrato_contrato', on_delete=models.CASCADE )
-    local_ativo = models.CharField(max_length=200)
-    cpf_cnpj = models.CharField(max_length=200)
-    data_inicio_contrato = models.DateField()
-    data_fim_contrato = models.DateField()
-    vigencia_primeiro_contrato = models.CharField(max_length=200)
-    vigencia_aditivo_atual = models.CharField(max_length=200)
-    clausula_renovacao = models.BooleanField(default=False)
-    clausula_restritiva_de_uso  = models.BooleanField(default=False)
-    clausula_recisao_entra_partes  = models.BooleanField(default=False)
-    incentivos_recebidos  = models.BooleanField(default=False)
-    valor_incentivos = models.CharField(max_length=200)
-'''
-
-
 class ContratoLocacaoForm(forms.ModelForm):
 
     class Meta:
